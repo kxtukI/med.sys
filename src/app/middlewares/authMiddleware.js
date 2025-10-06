38d4a7aa29a219baec0e78e2ff3c82d02206cac8
Felipe Katuki <felipekmkatuki@gmail.com>
2025-10-06 19:11:05 -0300

    implementing logout

--- src/app/middlewares/authMiddleware.js
+++ src/app/middlewares/authMiddleware.js
@@ -1,10 +1,9 @@
 import jwt from "jsonwebtoken";
 import { promisify } from 'util';
-
+import { tokenBlacklist } from '../controllers/SessionsController.js';
 import authConfig from '../../config/auth.js';
 
 export default async (req, res, next) => {
-    
     const authHeader = req.headers.authorization;
 
     if(!authHeader) {
@@ -13,11 +12,13 @@ export default async (req, res, next) => {
 
     const [, token] = authHeader.split(" ");
 
+    if (tokenBlacklist.has(token)) {
+        return res.status(401).json({ error: "Token inválido" });
+    }
+
     try {
         const decoded = await promisify(jwt.verify)(token, authConfig.secret);
-
         req.userId = decoded.id;
-
         return next();
     } catch (error) {
         return res.status(401).json({error: "Token inválido"});
